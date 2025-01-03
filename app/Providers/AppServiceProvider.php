9529e70eb3afe226d3020b8b80bd545b7b85cbcb
rdtyaaa <116392524+rdtyaaa@users.noreply.github.com>
2025-01-03 16:30:46 +0700

    dont know

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setLocale('id');
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
