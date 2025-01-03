65bd62be0794705b85d472b00d03fb156e65e103
rdtyaaa <116392524+rdtyaaa@users.noreply.github.com>
2025-01-03 08:21:16 +0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,6 @@ import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
-        viteStaticCopy({
-            targets: [
-                {
-                    src: "resources/assets/img", // Sumber gambar
-                    dest: "./", // Folder tujuan di build
-                },
-            ],
-        }),
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
